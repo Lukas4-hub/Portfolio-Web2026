4c56eec66a2d8d3d1a4c8e918d908a0975b300ee
Lucas <lbeltrame0@gmail.com>
2026-02-05 11:53:06 -0300

    Proyecto nuevo

--- src/data/proyectos.js
+++ src/data/proyectos.js
@@ -10,9 +10,17 @@ export const proyectos = [
       slug: "Onsen-Spa",
       titulo: "Pagina de reservas para Spa ficticio",
       descripcion: "Aplicación web de una SPA ficticia que permite a los usuarios reservar servicios de manera fácil y rápida, mientras que los administradores pueden gestionar reservas, controlar los servicios disponibles y administrar los perfiles de los usuarios. Incluye un sistema de roles que diferencia entre usuarios comunes y administradores, ofreciendo funcionalidades exclusivas para cada tipo de usuario. La plataforma está diseñada para mejorar la experiencia tanto de los clientes como del personal de la SPA, centralizando toda la gestión en una interfaz moderna y accesible.",
-      tecnologias: ["HTML", "CSS","JS","PHP","BOOTSTRAP"],
+      tecnologias: ["Html", "Css","Js","Php","Bootstrap"],
       link: "https://github.com/Lukas4-hub/Onsen-Spa"
     },
+    {
+      slug: "Event-Platfform",
+      titulo: "Event-Platfform",
+      descripcion: "App full stack para eventos: organizadores crean y publican eventos con ediciones y cupos; asistentes se registran/cancelan y ven “Mis registros”. Incluye login con roles (RBAC), control de cupos y base en Postgres con Prisma.",
+      tecnologias: ["Next.js","TypeScript","Tailwind CSS","Docker","Prisma"],
+      link: "https://github.com/Lukas4-hub/event-plattform"
+    },
+    
  
   
   ];
